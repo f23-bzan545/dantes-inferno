70d55034e85cadfea4d319ef3abfda3811b58956
Adam Spannbauer <SpannbauerAdam@gmail.com>
2023-08-31 08:14:20 -0400

    add solutionfile

---
+++ solution.sh
@@ -0,0 +1,90 @@
+# navigate into limbo folder
+cd limbo
+
+# navigate into castle walls
+# use tab key to help
+cd castle-wall/castle-wall/castle-wall/castle-wall/castle-wall/castle-wall/castle-wall/
+
+# list files in castle-walls
+ls
+
+# print one of the files
+cat aristotle
+
+# navigate back to limbo
+cd ../../../../../../../
+
+# navigate into lust folder
+cd lust
+
+# list all files even hidden
+ls -a
+
+# print out a hidden file
+cat .achilles
+
+# navigate to gluttony folder
+cd gluttony
+
+# print first line of ciacco file
+head -n 1 ciacco
+
+# print last line of ciacco file
+tail -n 1 ciacco
+
+# both at once (beware, its v fancy)
+(head -n 1; tail -n 1) < ciacco
+
+# navigate to greed 
+cd greed
+
+# create files (many ways to do this!!)
+touch spenders
+touch hoarders
+
+# give joust program execute permissions
+chmod +x ./joust
+
+# run joust program
+./joust
+
+# make a directory called dis
+mkdir dis
+
+# move boat into dis
+mv boat dis
+
+# unzip zip file
+unzip tombs.zip
+
+# navigate to violence
+cd violence
+
+# delete minotaur file
+rm minotaur
+
+# navigate to fraud
+cd fraud
+
+# copy lead file
+cp lead gold
+
+# edit lead file (many ways to do this!)
+#
+# use: `code lead` if you want to use vscode to edit
+# (google code command if it doesnt work)
+# 
+# using nano
+nano gold
+# use arrow keys to move cursor where you want
+# use backspace and keys to type
+# CTRL+O to save your work (write Out)
+#     you can change name after CTRL+O
+#     but don't change here; just ENTER to save
+# CTRL+X to exit nano
+
+# navigate to treachery
+cd treachery
+
+# remove the icy-lake dir and it's contents
+rm -r icy-lake
